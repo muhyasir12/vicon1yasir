a823a4bb74213acc51379c49b6a99c8eb96ccdc4
muhyasir12 <101491766+muhyasir12@users.noreply.github.com>
2022-03-16 19:59:37 +0700

    Add files via upload

---
+++ vicon1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class vicon1 {
+    public static void main (String[] args ) {
+        String [][]names = {
+            {"Soal ","Jawaban"},
+            {"Pemograman Java","Algoritma & Pemograman","Komputer Grafik"}
+        };
+        System.out.println(names[1][2] + names [1][0]); // Soal Pemoggraman java
+        //System.out.println(names[0][1] + names [1][2]); // jawaban komputer grafik
+        }
+    }
+    
+
